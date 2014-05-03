ab68efb5d3a4e3508a42e8c49935bbffb396db7d
rendyep <rendyekoprastiyo@gmail.com>
2014-05-03 08:47:56 +0700

    change the way autoloader works

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,32 +1,3 @@
 <?php
 
-use Doctrine\ORM\Tools\Setup;
-use Doctrine\ORM\EntityManager;
-
-if (!isset($loader)) {
-    $loader = include(__DIR__ . "/../../../autoload.php");
-}
-
-$loader->set("WWII\\Domain", __DIR__ . '/../../domain/src/');
-$loader->register(true);
-$loader->set("WWII\\Application", __DIR__ . '/../../application/src/');
-$loader->register(true);
-$loader->set("WWII\\Console", __DIR__ . '/../../console/src/');
-$loader->register(true);
-$loader->set("WWII\Common", __DIR__ . '/../../common/src');
-$loader->register(true);
-$loader->set("WWII\\", __DIR__ . '/../../core/src/');
-$loader->register(true);
-
-$isDevMode = true;
-$configManager = \WWII\Config\ConfigManager::getInstance();
-$config = Setup::createXMLMetadataConfiguration($configManager->get('doctrine_mappings'), $isDevMode);
-$connection = $configManager->get('database');
-
-$entityManager = EntityManager::create($connection, $config);
-$platform = $entityManager->getConnection()->getDatabasePlatform();
-$platform->registerDoctrineTypeMapping('enum', 'string');
-
-$configManager = \WWII\Config\ConfigManager::getInstance();
-$serviceManager = new \WWII\Service\ServiceManager();
-$serviceManager->setConfigManager($configManager);
+include(__DIR__ . '/../../init_autoload.php');
