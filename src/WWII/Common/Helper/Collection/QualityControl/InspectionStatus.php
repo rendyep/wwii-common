07079e6c3addd7fcc9167c9edef55328ba64e265
rendyep <rendyekoprastiyo@gmail.com>
2014-07-03 15:24:42 +0700

    set countable item of 'Quality Control - Inspection Status' based on group

--- src/WWII/Common/Helper/Collection/QualityControl/InspectionStatus.php
+++ src/WWII/Common/Helper/Collection/QualityControl/InspectionStatus.php
@@ -17,23 +17,32 @@ class InspectionStatus implements \WWII\Common\Helper\HelperCollectionInterface
         $this->databaseManager = $serviceManager->get('DatabaseManager');
     }
 
-    public function getInspectionStatus(
-        \WWII\Domain\Erp\QualityControl\GeneralInspection\DailyInspectionItem $dailyInspectionItem
-    ) {
+    public function getInspectionStatus($inspectionItem) {
+        if (! $inspectionItem instanceOf \WWII\Domain\Erp\QualityControl\GeneralInspection\AssemblingInspectionItem
+            && ! $inspectionItem instanceOf \WWII\Domain\Erp\QualityControl\GeneralInspection\FinishingInspectionItem
+            && ! $inspectionItem instanceOf \WWII\Domain\Erp\QualityControl\GeneralInspection\PackagingInspectionItem
+        ) {
+            throw new \Exception('InspectionItem must be instance of one on the list:'
+                . PHP_EOL . '\WWII\Domain\Erp\QualityControl\GeneralInspection\AssemblingInspectionItem'
+                . PHP_EOL . '\WWII\Domain\Erp\QualityControl\GeneralInspection\FinishingInspectionItem'
+                . PHP_EOL . '\WWII\Domain\Erp\QualityControl\GeneralInspection\PackagingInspectionItem'
+                );
+        }
+
         $acceptanceLimit = $this->entityManager->createQueryBuilder()
             ->select('acceptanceLimit')
             ->from('WWII\Domain\Erp\QualityControl\GeneralInspection\AcceptanceLimit', 'acceptanceLimit')
             ->leftJoin('acceptanceLimit.acceptanceIndex', 'acceptanceIndex')
             ->where('acceptanceIndex.code = :acceptanceIndex')
-                ->setParameter('acceptanceIndex', $dailyInspectionItem->getAcceptanceIndex())
+                ->setParameter('acceptanceIndex', $inspectionItem->getAcceptanceIndex())
             ->andWHere('acceptanceLimit.sampleSize = :sampleSize')
-                ->setParameter('sampleSize', $dailyInspectionItem->getJumlahInspeksi())
+                ->setParameter('sampleSize', $inspectionItem->getJumlahInspeksi())
             ->getQuery()
             ->setFirstResult(0)
             ->setMaxResults(1)
             ->getOneOrNullResult();
 
-        $jumlahItemBuruk = $dailyInspectionItem->getJumlahTotalItemBuruk();
+        $jumlahItemBuruk = $inspectionItem->getJumlahTotalItemBuruk();
         $minJumlahItemBuruk = $acceptanceLimit->getLimit();
 
         return $jumlahItemBuruk <= $minJumlahItemBuruk;
